df22ead82df67ee102de05cc24647672da074256
Sumedhasd <sumedhasd28@gmail.com>
2021-09-15 11:54:39 +0530

    Handson 3added

---
+++ Handson3/Class2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Handson3
+{
+    class Class2
+    {
+        public string Readfile()
+
+        {
+            Console.WriteLine("Mobile Network performance");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("Source" + "\t\t" + "Destination \tDate and time\t\tcall-status");
+            FileStream fs = new FileStream(@"C:\Users\Sumedha Desai\Training\demo folder\file handling\network.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            int i = 0;
+            while (sr.Peek() > 0)
+            {
+                string line = sr.ReadLine();
+                if (line.Contains(":"))
+                {
+
+                    string[] data = line.Split(':');
+                    if (data.Length > 2)
+                    {
+                        Console.Write(data[1] + ":" + data[2] + ":" + data[3] + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(data[1] + "\t");
+                    }
+                    i++;
+                    if (i % 4 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+
+
+            }
+            return "";
+        }
+    }
+}
---
+++ Handson3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 objfile = new Class2();
+            string a = objfile.Readfile();
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
