5ce8b4b8c0d7e085530e01bfb86046e43c078ce1
Sumedhasd <sumedhasd28@gmail.com>
2021-09-15 11:44:59 +0530

    Handson 2added

---
+++ Handon2/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Handon2
+{
+    class Class1
+    {
+
+        int n;
+        string r;
+        string c;
+        string ct;
+        public string Writefile()
+        {
+
+            Console.WriteLine("Course details:");
+            Console.WriteLine("Enter no of courses:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            FileStream fs = new FileStream(@"C:\Users\Sumedha Desai\Training\demo folder\file handling\test.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Course details:");
+            sw.WriteLine("Enter the no of courses:" + n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Regno:");
+                r = Console.ReadLine();
+                Console.WriteLine("Enter Course:");
+
+                c = Console.ReadLine();
+                Console.WriteLine("Enter Course title:");
+                ct = Console.ReadLine();
+                Console.WriteLine();
+
+
+                sw.WriteLine("Reg no:" + r);
+                sw.WriteLine("course:" + c);
+                sw.WriteLine("coursetitle:" + ct);
+
+
+
+            }
+            sw.Close();
+            fs.Close();
+
+
+
+
+
+            return " welcome";
+        }
+    }
+}
---
+++ Handon2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handon2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.Writefile();
+            Console.Read();
+
+
+
+
+
+
+            //int v1, v2, v3, v4, v5, v6;
+            //Console.WriteLine("Enter the value1:");
+            //v1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the value2:");
+            //v2 = Convert.ToInt32(Console.ReadLine());
+            //v3 = v1 + v2;
+            //v4 = v1 - v2;
+            //v5 = v1 * v2;
+            //v6 = v1 / v2;
+            //Console.WriteLine("Sum:" + v3);
+            //Console.WriteLine("Diff:" + v4);
+            //Console.WriteLine("Product:" + v5);
+            //Console.WriteLine("Quotient:" + v6);
+            //Console.WriteLine("Quotient:" + v1 / v2);
+        }
+    }
+}
