04bbd913ca840a6e5dc3c33e27bb7e4de1679db1
Sumedhasd <sumedhasd28@gmail.com>
2021-09-20 08:02:21 +0530

    Assignment added

---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float weight;
+            string pclass;
+            float amount=0;
+            Console.WriteLine("Total Weight of the Luggage:");
+            weight=float.Parse(Console.ReadLine());
+          
+            Console.WriteLine("Enter the class of the passenger:");
+            pclass = Console.ReadLine().ToLower();
+            
+            if (pclass =="first class" )
+            {
+                if (weight > 50)
+                {
+                    amount = (weight - 50) * 2;
+                }
+            }
+            else if (pclass == "business class")
+            {
+                if (weight > 40)
+                {
+                    amount = (weight - 40) * 2;
+                }
+            }
+            else
+            {
+                if (weight > 20)
+                {
+                    amount = (weight - 20) * 2;
+                }
+            }
+            Console.WriteLine(amount);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ asssignment1/asssignment1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class bookTickets
+    {
+       public void ticketbooking()
+    {
+        Console.WriteLine("normal ticket booked");
+    }
+      }
+public class bookEtickets : bookTickets
+{
+    public void eticketbooking()
+    {
+        Console.WriteLine("e ticket booked");
+    }
+}
+public class viewMaintenance
+    {
+    public void details()
+    {
+        Console.WriteLine("Maintenance Details ");
+    }
+}
+
+
+namespace asssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bookEtickets obj = new bookEtickets();
+            Console.WriteLine("Do you want to book normal ticket or e ticket:");
+            string response = Console.ReadLine();
+            if (response == "normal ticket")
+
+            { obj.ticketbooking();
+            }
+            else
+            { 
+                        obj.eticketbooking();
+        }
+
+            viewMaintenance obj1 = new viewMaintenance();
+            obj1.details();
+
+            Console.ReadLine();
+        }
+    }
+}
