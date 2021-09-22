c958b85e324f2573025134d3af681c5e54187b7a
Sumedhasd <sumedhasd28@gmail.com>
2021-09-22 09:02:14 +0530

    python code on dictionary added

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+Dict ={ 1:"Player5" , 2:"Player1", 3:"Player3", 4:"Player2", 5:"Player4"}
+print(len(Dict.keys()))
+
+Dict[1]="Player4"
+Dict[5]="Player5"
+j=1
+for i in Dict:
+    print(j,Dict[j])
+    j=j+1
+
+    
+
